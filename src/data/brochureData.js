a6bfd32fffaf9fe0e54053d4d04656f4bdc52056
moazabdulhalim <moazabdulhalim@gmail.com>
2023-11-07 12:49:23 +0300

    fixes

---
+++ src/data/brochureData.js
@@ -0,0 +1,12 @@
+import Brochure1 from "../assets/images/Brochure.webp";
+import BrochurePdf1 from "../assets/pdf/330.pdf";
+
+export const data = [
+  {
+    template: 1,
+    photo: Brochure1,
+    title: "brochurDownload",
+    file: BrochurePdf1,
+  },
+  ,
+];
